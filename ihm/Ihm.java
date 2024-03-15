988c4ccfa770fd6ef78fb47dde744ffc2a68c5ef
FlorianB30 <96230889+FlorianB30@users.noreply.github.com>
2024-03-15 22:23:35 +0100

    first commit

---
+++ ihm/Ihm.java
@@ -0,0 +1,83 @@
+package ihm;
+
+import java.util.Scanner;
+
+import pieces.Piece;
+
+public class Ihm implements Iihm {
+
+	@Override
+	public String askNamePlayer(int numJ) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Quel est le nom du joueur " + numJ + " ?");
+		return sc.next();
+	}
+
+	@Override
+	public String askColorPlayer(int numJ) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Quel est la couleur (b ou r) du joueur " + numJ + " ?");
+		return sc.next();
+	}
+
+	@Override
+	public int askXpos(String namePlayer) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(namePlayer + " entrez la colonne");	
+		return sc.nextInt();
+	}
+
+	@Override
+	public int askYpos(String namePlayer) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(namePlayer + " entrez la ligne");	
+		return sc.nextInt();
+	}
+
+	@Override
+	public void affiche(Piece[][] plat) {
+		String s = ligne(plat.length) + "\n" + colonne(plat) + ligne(plat.length);
+		System.out.println(s);
+	}
+	
+	private String ligne(int taille) {
+		String s ="  ";
+		for (int i = 0; i < taille; ++i) {
+			s += "   "; //" r ";
+		}
+		return s;
+	}
+	
+	private String colonne(Piece[][] plat) {
+		String s = "";
+		String decal = " ";
+		for(int y = 0; y < plat.length; ++y) {
+			s += y + decal ; //+ "b";
+			for(int x = 0; x < plat.length; ++x) {
+				s += " ";
+				if(plat[y][x] != null) {
+					s += plat[y][x].getColor();
+				} else {
+					s += ".";
+				}
+				s += " ";
+			}
+			s += "\n"; //"b\n";
+			decal = space(decal, y);
+		}
+		s += decal;
+		return s;
+	}
+
+	private String space(String decal, int num) {
+		if(num != 9) {
+			decal += " ";
+		}
+		return decal;
+	}
+	
+	@Override
+	public void xyErreur() {
+		System.out.println("Les coordonn�es entr�es ne sont pas bonnes");
+	}
+}
---
+++ plateau/Plateau.java
@@ -0,0 +1,38 @@
+package plateau;
+
+import pieces.Piece;
+import player.Player;
+
+public class Plateau {
+	private int taille;
+	private Piece[][] plateau;
+	
+	public Plateau(int taille) {
+		this.taille = taille;
+		this.plateau = new Piece[this.taille][this.taille];
+	}
+	
+	public void init() {
+		for(int y = 0; y < this.taille; ++y) {
+			for(int x = 0; x < this.taille; ++x) {
+				this.plateau[y][x] = null;
+			}
+		}
+	}
+	
+	public Piece[][] getPlateau() {
+		return this.plateau;
+	}
+	
+	public void place(int x, int y, Piece p) {
+		this.plateau[y][x] = p;
+	}
+
+	public boolean xyValidation(int x, int y) {
+		return (0 < x && x < this.taille && 0 <= y && y < this.taille );
+	}
+
+	public boolean caseVide(int x, int y) {
+		return this.plateau[y][x] != null;
+	}
+}
